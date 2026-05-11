4c28d88941c049cca7c5372a138ed0bc2e7357a0
Sridhar Ratnakumar <srid@srid.ca>
2026-05-11 18:55:37 -0400

    feat: scaffold standalone Haskell hello-world with flake + CI
    Adds a minimal Haskell executable using haskell-flake's standalone
    (non-flake-parts) API. CI runs on a self-hosted x86_64-linux runner
    that already has Nix installed.

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+
+  outputs = { self, nixpkgs, haskell-flake, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          project = (haskell-flake.lib { inherit pkgs; }).evalHaskellProject {
+            projectRoot = self;
+          };
+        in
+        {
+          packages.default = project.packages.ci.package;
+          devShells.default = project.devShell;
+        };
+
+      systemOutputs = eachSystem perSystem;
+    in
+    {
+      packages = nixpkgs.lib.mapAttrs (_: s: s.packages) systemOutputs;
+      devShells = nixpkgs.lib.mapAttrs (_: s: s.devShells) systemOutputs;
+    };
+}
